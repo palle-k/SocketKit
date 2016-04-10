8a43b4bf61edcb3071ee1a37214ba8669bdb654d
Palle <palleklewitz@icloud.com>
2016-04-10 22:18:47 +0200

    Initial Commit

---
+++ SocketKit/SocketKit.h
@@ -0,0 +1,19 @@
+//
+//  SocketKit.h
+//  SocketKit
+//
+//  Created by Palle Klewitz on 10.04.16.
+//  Copyright © 2016 Palle Klewitz. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SocketKit.
+FOUNDATION_EXPORT double SocketKitVersionNumber;
+
+//! Project version string for SocketKit.
+FOUNDATION_EXPORT const unsigned char SocketKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SocketKit/PublicHeader.h>
+
+
