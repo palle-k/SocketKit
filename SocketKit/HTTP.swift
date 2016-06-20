13a9ad7e8ba091766087accacd7e3777941ce296
Palle <palleklewitz@icloud.com>
2016-06-20 04:39:32 +0200

    Added HTTP Status Codes

--- SocketKit/HTTP.swift
+++ SocketKit/HTTP.swift
@@ -28,10 +28,10 @@ import Foundation
 
 /**
 
-Date formatter for HTML time format
+Date formatter for HTTP time format
 
 */
-private let DateFormatter:NSDateFormatter =
+internal let HTTPDateFormatter:NSDateFormatter =
 {
 	let formatter = NSDateFormatter()
 	formatter.dateFormat = "EEE, d MMM yyyy HH:mm:ss zzz"
@@ -877,6 +877,638 @@ public enum HTTPResponseHeaderField: String
 }
 
 
+public enum HTTPStatusCode : String
+{
+	//1XX - Informational
+	
+	/**
+
+	The server has received the request headers and the client 
+	should proceed to send the request body 
+	(in the case of a request for which a body needs to be sent; for example, a POST request). 
+	
+	Sending a large request body to a server after a request has been rejected
+	for inappropriate headers would be inefficient. 
+	To have a server check the request's headers, 
+	a client must send Expect: 100-continue as a header in its 
+	initial request and receive a 100 Continue status code in response before sending the body. 
+	The response 417 Expectation Failed indicates the request should not be continued
+	
+	*/
+	case Continue = "100 Continue"
+	
+	
+	/**
+	
+	The requester has asked the server to switch protocols and the server has agreed to do so.
+	
+	*/
+	case SwitchingProtocols = "101 Switching Protocols"
+	
+	
+	/**
+	
+	A WebDAV request may contain many sub-requests involving file operations, 
+	requiring a long time to complete the request. 
+	
+	This code indicates that the server has received and is processing the request, 
+	but no response is available yet.
+ 
+	This prevents the client from timing out and assuming the request was lost.
+	
+	*/
+	case Processing = "102 Processing"
+	
+	
+	//2XX - Success
+	
+	/**
+	
+	Standard response for successful HTTP requests. 
+	
+	The actual response will depend on the request method used. 
+	
+	- In a GET request, the response will contain an entity corresponding to the requested resource.
+	- In a POST request, the response will contain an entity describing or containing the result of the action.
+	
+	*/
+	case OK = "200 OK"
+	
+	
+	/**
+	
+	The request has been fulfilled, resulting in the creation of a new resource.
+	
+	*/
+	case Created = "201 Created"
+	
+	
+	/**
+	
+	The request has been accepted for processing, but the processing has not been completed. 
+	The request might or might not be eventually acted upon, 
+	and may be disallowed when processing occurs.
+	
+	*/
+	case Accepted = "202 Accepted"
+	
+	
+	/**
+	
+	The server is a transforming proxy (e.g. a Web accelerator) that received a 200 OK from its origin, 
+	but is returning a modified version of the origin's response.
+	
+	*/
+	case NonAuthoritativeInformation = "203 Non-Authoritative Information"
+	
+	
+	/**
+	
+	The server successfully processed the request and is not returning any content.
+	
+	*/
+	case NoContent = "204 No Content"
+	
+	
+	/**
+	
+	The server successfully processed the request, but is not returning any content. 
+	Unlike a 204 response, this response requires that the requester reset the document view.
+	
+	*/
+	case ResetContent = "205 Reset Content"
+	
+	
+	/**
+	
+	The server is delivering only part of the resource (byte serving) 
+	due to a range header sent by the client.
+	
+	The range header is used by HTTP clients to enable resuming of interrupted downloads, 
+	or split a download into multiple simultaneous streams.
+	
+	*/
+	case PartialContent = "206 Partial Content"
+	
+	
+	/**
+	
+	The message body that follows is an XML message and can contain
+	a number of separate response codes,
+	depending on how many sub-requests were made.
+	
+	*/
+	case MultiStatus = "207 Multi-Status"
+	
+	
+	/**
+	
+	The members of a DAV binding have already been enumerated in a previous reply to this request, 
+	and are not being included again.
+	
+	*/
+	case AlreadyReported = "208 Already Reported"
+	
+	
+	/**
+	
+	The server has fulfilled a request for the resource, 
+	and the response is a representation of the result of 
+	one or more instance-manipulations applied to the current instance.
+	
+	*/
+	case IMUsed = "226 IM Used"
+	
+	
+	//3XX - Redirect
+	
+	/**
+	
+	Indicates multiple options for the resource from which the client may choose. 
+	
+	For example, this code could be used to present multiple video format options, 
+	to list files with different extensions, or to suggest word sense disambiguation.
+	
+	*/
+	case MultipleChoices = "300 Multiple Choices"
+	
+	
+	/**
+	
+	This and all future requests should be directed to the given URI.
+	
+	*/
+	case MovedPermanently = "301 Moved Permanently"
+	
+	
+	/**
+	
+	The HTTP/1.0 specification (RFC 1945) required the client to perform a temporary redirect 
+	(the original describing phrase was "Moved Temporarily"),
+	but popular browsers implemented 302 with the functionality of a 303 See Other. 
+	Therefore, HTTP/1.1 added status codes 303 and 307 to distinguish between the two behaviours.
+	
+	However, some Web applications and frameworks use the 302 status code as if it were the 303.
+	
+	*/
+	case Found = "302 Found"
+	
+	
+	/**
+	
+	The response to the request can be found under another URI using a GET method. 
+	
+	When received in response to a POST (or PUT/DELETE), 
+	the client should presume that the server has received the data and should issue 
+	a redirect with a separate GET message.
+	
+	*/
+	case SeeOther = "303 See Other"
+	
+	
+	/**
+	
+	Indicates that the resource has not been modified since 
+	the version specified by the request headers If-Modified-Since or If-None-Match. 
+	
+	In such case, there is no need to retransmit the resource since 
+	the client still has a previously-downloaded copy.
+	
+	*/
+	case NotModified = "304 Not Modified"
+	
+	
+	/**
+	
+	The requested resource is available only through a proxy, 
+	the address for which is provided in the response. 
+	
+	Many HTTP clients (such as Mozilla and Internet Explorer) do not correctly handle 
+	responses with this status code, primarily for security reasons.
+	
+	*/
+	case UseProxy = "305 Use Proxy"
+	
+	
+	/**
+	
+	No longer used. Originally meant "Subsequent requests should use the specified proxy."
+	
+	*/
+	@available(*, deprecated=0.1)
+	case SwitchProxy = "306 Switch Proxy"
+	
+	
+	/**
+	
+	In this case, the request should be repeated with another URI; however, 
+	future requests should still use the original URI. 
+	In contrast to how 302 was historically implemented, 
+	the request method is not allowed to be changed when reissuing the original request. 
+	
+	For example, a POST request should be repeated using another POST request.
+	
+	*/
+	case TemporaryRedirect = "307 Temporary Redirect"
+	
+	
+	/**
+	
+	The request and all future requests should be repeated using another URI. 
+	307 and 308 parallel the behaviours of 302 and 301, but do not allow the HTTP method to change. 
+	So, for example, submitting a form to a permanently redirected resource may continue smoothly.
+	
+	*/
+	case PermanentRedirect = "308 Permanent Redirect"
+	
+	
+	//4XX - Client Error
+	
+	/**
+	
+	The server cannot or will not process the request due to an apparent client error 
+	(e.g., malformed request syntax, invalid request message framing, or deceptive request routing).
+	
+	*/
+	case BadRequest = "400 Bad Request"
+	
+	
+	/**
+	
+	Similar to 403 Forbidden, but specifically for use when authentication is required 
+	and has failed or has not yet been provided.
+	The response must include a WWW-Authenticate header field containing 
+	a challenge applicable to the requested resource. 
+	
+	401 semantically means "unauthenticated", i.e. the user does not have the necessary credentials.
+	
+	Note: Some sites issue HTTP 401 when an IP address is banned from the website 
+	(usually the website domain) and that specific address is refused permission to access a website.
+	
+	*/
+	case Unauthorized = "401 Unauthorized"
+	
+	
+	/**
+	
+	Reserved for future use. The original intention was that this code 
+	might be used as part of some form of digital cash or micropayment scheme, 
+	but that has not happened, and this code is not usually used. 
+	Google Developers API uses this status if a particular developer has exceeded the daily limit on requests.
+	
+	*/
+	case PaymentRequired = "402 Payment Required"
+	
+	
+	/**
+	
+	The request was a valid request, but the server is refusing to respond to it. 
+	403 error semantically means "unauthorized", i.e. the user does not have 
+	the necessary permissions for the resource.
+	
+	*/
+	case Forbidden = "403 Forbidden"
+	
+	
+	/**
+	
+	The requested resource could not be found but may be available in the future. 
+	Subsequent requests by the client are permissible.
+	
+	*/
+	case NotFound = "404 Not Found"
+	
+	
+	/**
+	
+	A request method is not supported for the requested resource; for example, 
+	a GET request on a form which requires data to be presented via POST, 
+	or a PUT request on a read-only resource.
+	
+	*/
+	case MethodNotAllowed = "405 Method Not Allowed"
+	
+	
+	/**
+	
+	The requested resource is capable of generating only content 
+	not acceptable according to the Accept headers sent in the request.
+	
+	*/
+	case NotAcceptable = "406 Not Acceptable"
+	
+	
+	/**
+	
+	The client must first authenticate itself with the proxy.
+	
+	*/
+	case ProxyAuthenticationRequired = "407 Proxy Authentication Required"
+	
+	
+	/**
+	
+	The server timed out waiting for the request. 
+	
+	According to HTTP specifications: 
+	"The client did not produce a request within the time that the server was prepared to wait. 
+	The client MAY repeat the request without modifications at any later time."
+	
+	*/
+	case RequestTimeout = "408 Request Timeout"
+	
+	
+	/**
+	
+	Indicates that the request could not be processed because of conflict in the request, 
+	such as an edit conflict between multiple simultaneous updates.
+	
+	*/
+	case Conflict = "409 Conflict"
+	
+	
+	/**
+	
+	Indicates that the resource requested is no longer available and will not be available again. 
+	This should be used when a resource has been intentionally removed and the resource should be purged. 
+	Upon receiving a 410 status code, the client should not request the resource in the future. 
+	Clients such as search engines should remove the resource from their indices.
+	Most use cases do not require clients and search engines to purge the resource, 
+	and a "404 Not Found" may be used instead.
+	
+	*/
+	case Gone = "410 Gone"
+	
+	
+	/**
+	
+	The request did not specify the length of its content, 
+	which is required by the requested resource.
+	
+	*/
+	case LengthRequired = "411 Length Required"
+	
+	
+	/**
+	
+	The server does not meet one of the preconditions that the requester put on the request.
+	
+	*/
+	case PreconditionFailed = "412 Precondition Failed"
+	
+	
+	/**
+	
+	The request is larger than the server is willing or able to process. 
+	Previously called "Request Entity Too Large".
+	
+	*/
+	case PayloadTooLarge = "413 Payload Too Large"
+	
+	
+	/**
+	
+	The URI provided was too long for the server to process. 
+	Often the result of too much data being encoded as a query-string of a GET request, 
+	in which case it should be converted to a POST request.
+	
+	Called "Request-URI Too Long" previously.
+	
+	*/
+	case URITooLong = "414 URI Too Long"
+	
+	
+	/**
+	
+	The request entity has a media type which the server or resource does not support. 
+	For example, the client uploads an image as image/svg+xml, 
+	but the server requires that images use a different format.
+	
+	*/
+	case UnsupportedMediaType = "415 Unsupported Media Type"
+	
+	
+	/**
+	
+	The client has asked for a portion of the file (byte serving),
+	but the server cannot supply that portion. 
+	
+	For example, if the client asked for a part of the file that lies beyond the end of the file.
+	
+	Called "Requested Range Not Satisfiable" previously.
+	
+	*/
+	case RangeNotSatisfiable = "416 Range Not Satisfiable"
+	
+	
+	/**
+	
+	The server cannot meet the requirements of the Expect request-header field.
+	
+	*/
+	case ExpectationFailed = "417 Expectation Failed"
+	
+	
+	/**
+	
+	Used in Hyper Text Coffee Pot Control Protocol (HTCPCP).
+	
+	Should be returned by tea pots requested to brew coffee.
+	
+	*/
+	case ImATeapot = "418 I'm a teapot"
+	
+	
+	/**
+	
+	The request was directed at a server that is not able to produce a response 
+	(for example because a connection reuse).
+	
+	*/
+	case MisdirectedRequest = "421 Misdirected Request"
+	
+	
+	/**
+	
+	The request was well-formed but was unable to be followed due to semantic errors.
+	
+	*/
+	case UnprocessableEntity = "422 Unprocessable Entity"
+	
+	
+	/**
+	
+	The resource that is being accessed is locked.
+	
+	*/
+	case Locked = "423 Locked"
+	
+	
+	/**
+	
+	The request failed due to failure of a previous request (e.g., a PROPPATCH).
+	
+	*/
+	case FailedDependency = "424 Failed Dependency"
+	
+	
+	/**
+	
+	The client should switch to a different protocol such as TLS/1.0, 
+	given in the Upgrade header field.
+	
+	*/
+	case UpgradeRequired = "426 Upgrade Required"
+	
+	
+	/**
+	
+	The origin server requires the request to be conditional. 
+	
+	Intended to prevent "the 'lost update' problem, 
+	where a client GETs a resource's state, modifies it, 
+	and PUTs it back to the server, 
+	when meanwhile a third party has modified the state on the server, 
+	leading to a conflict."
+	
+	*/
+	case PreconditionRequired = "428 Precondition Required"
+	
+	
+	/**
+	
+	The user has sent too many requests in a given amount of time. 
+	
+	Intended for use with rate limiting schemes.
+	
+	*/
+	case TooManyRequests = "429 Too Many Requests"
+	
+	
+	/**
+	
+	The server is unwilling to process the request because either an individual header field, 
+	or all the header fields collectively, are too large.
+	
+	*/
+	case RequestHeaderFieldsTooLarge = "431 Request Header Fields Too Large"
+	
+	
+	/**
+	
+	A server operator has received a legal demand to deny access to a resource 
+	or to a set of resources that includes the requested resource.
+	
+	The code 451 was chosen as a reference to the novel Fahrenheit 451.
+	
+	*/
+	case UnavailableForLegalReasons = "451 Unavailable For Legal Reasons"
+	
+	
+	//5XX - Server Error
+	
+	/**
+	
+	A generic error message, given when an unexpected condition 
+	was encountered and no more specific message is suitable.
+	
+	*/
+	case InternalServerError = "500 Internal Server Error"
+	
+	
+	/**
+	
+	The server either does not recognize the request method, 
+	or it lacks the ability to fulfill the request. 
+	
+	Usually this implies future availability (e.g., a new feature of a web-service API).
+	
+	*/
+	case NotImplemented = "501 Not Implemented"
+	
+	
+	/**
+	
+	The server was acting as a gateway or proxy and 
+	received an invalid response from the upstream server.
+	
+	*/
+	case BadGateway = "502 Bad Gateway"
+	
+	
+	/**
+	
+	The server is currently unavailable (because it is overloaded or down for maintenance).
+	Generally, this is a temporary state.
+	
+	*/
+	case ServiceUnavailable = "503 Service Unavailable"
+	
+	
+	/**
+	
+	The server was acting as a gateway or proxy and did not receive 
+	a timely response from the upstream server.
+	
+	*/
+	case GatewayTimeout = "504 Gateway Timeout"
+	
+	
+	/**
+	
+	The server does not support the HTTP protocol version used in the request.
+	
+	*/
+	case HTTPVersionNotSupported = "505 HTTP Version Not Supported"
+	
+	
+	/**
+	
+	Transparent content negotiation for the request results in a circular reference.
+	
+	*/
+	case VariantAlsoNegotiates = "506 Variant Also Negotiates"
+	
+	
+	/**
+	
+	The server is unable to store the representation needed to complete the request.
+	
+	*/
+	case InsufficientStorage = "507 Insufficient Storage"
+	
+	
+	/**
+	
+	The server detected an infinite loop while processing the request 
+	(sent in lieu of 208 Already Reported).
+	
+	*/
+	case LoopDetected = "508 Loop Detected"
+	
+	
+	/**
+	
+	Further extensions to the request are required for the server to fulfil it.
+	
+	*/
+	case NotExtended = "510 Not Extended"
+	
+	
+	/**
+	
+	The client needs to authenticate to gain network access. 
+	
+	Intended for use by intercepting proxies used to control access to the network
+	(e.g., "captive portals" used to require agreement to Terms of Service 
+	before granting full Internet access via a Wi-Fi hotspot).
+	
+	*/
+	case NetworkAuthenticationRequired = "511 Network Authentication Required"
+	
+}
+
+
 public struct HTTPRequest : StreamReadable, StreamWritable
 {
 	public init(byReadingFrom stream: InputStream) throws
@@ -903,32 +1535,47 @@ Basic HTTP response to a get request.
 Sends the given data as content to the client
 
 */
-public class HTTPResponse : StreamWritable
+public class HTTPResponse : StreamWritable, StreamReadable
 {
+	public let version:String
+	public let status:String
+	public var header:[String:String]
+	
 	public let content:NSData
-	public let mimeType:String
+	
+	required public init(byReadingFrom stream: InputStream) throws
+	{
+		content = NSData()
+		version = "HTTP/1.1"
+		status = "200 OK"
+		header = [:]
+	}
 	
 	public init(with content: String)
 	{
 		self.content = content.dataUsingEncoding(NSUTF8StringEncoding)!
-		self.mimeType = "text/html"
+		version = "HTTP/1.1"
+		status = "200 OK"
+		header = [:]
 	}
 	
-	public init(with content: NSData, mimeType: String)
+	public init(with content: NSData)
 	{
 		self.content = content
-		self.mimeType = mimeType
+		version = "HTTP/1.1"
+		status = "200 OK"
+		header = [:]
 	}
 	
 	public func write(to outputStream: OutputStream) throws
 	{
 		try outputStream.writeln("HTTP/1.1 200 OK")
-		try outputStream.writeln("Date: \(DateFormatter.stringFromDate(NSDate()))")
+		try outputStream.writeln("Date: \(HTTPDateFormatter.stringFromDate(NSDate()))")
 		try outputStream.writeln("Server: PK-SocketKit/0.1a (OS X/Swift)")
-		try outputStream.writeln("Last-Modified: \(DateFormatter.stringFromDate(NSDate()))")
+		try outputStream.writeln("Last-Modified: \(HTTPDateFormatter.stringFromDate(NSDate()))")
 		try outputStream.writeln("Accept-Ranges: bytes")
 		try outputStream.writeln("Connection: close")
-		try outputStream.writeln("Content-Type: \(mimeType)")
+		try outputStream.writeln("Content-Type: text/html")
 		try outputStream.writeln("Content-Length: \(content.length)")
 		try outputStream.writeln()
 		try outputStream.write(content)
